bc9b32d077c6d5a3bfcff2e3353b20d577f7e2f7
Ivelise Sola <solaivelise@yahoo.com>
2017-12-27 15:49:15 -0500

    responsive nav

--- resources/js/script.js
+++ resources/js/script.js
@@ -64,4 +64,19 @@ $(".js-wp-4").waypoint(function() {
     offset: "50%"
 });
 
+/* Mobile Navigtion */
+$(".js-nav-icon").click(function () {
+    var nav = $(".js-main-nav");
+    var icon = $(".js-nav-icon i");
+
+    nav.slideToggle(200);
+    if (icon.hasClass("ion-navicon-round")) {
+        icon.addClass("ion-close-round");
+        icon.removeClass("ion-navicon-round");
+    } else {
+        icon.addClass("ion-navicon-round");
+        icon.removeClass("ion-close-round");
+    }
+})
+
 });
