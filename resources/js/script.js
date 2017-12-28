a9bf5fa10e650ebe3a2f5c0841bc5565df140c7b
Ivelise Sola <solaivelise@yahoo.com>
2017-12-27 20:59:42 -0500

    google map

--- resources/js/script.js
+++ resources/js/script.js
@@ -42,41 +42,60 @@ $(document).ready(function () {
 
     /* Animation on Scroll */
 
-$(".js-wp-1").waypoint(function() {
-    $(".js-wp-1").addClass("animated fadeIn");
-}, {
-    offset: "50%"
-});
+    $(".js-wp-1").waypoint(function () {
+        $(".js-wp-1").addClass("animated fadeIn");
+    }, {
+            offset: "50%"
+        });
 
-$(".js-wp-2").waypoint(function() {
-    $(".js-wp-2").addClass("animated fadeInUp");
-}, {
-    offset: "50%"
-});
-$(".js-wp-3").waypoint(function() {
-    $(".js-wp-3").addClass("animated fadeIn");
-}, {
-    offset: "50%"
-});
-$(".js-wp-4").waypoint(function() {
-    $(".js-wp-4").addClass("animated pulse");
-}, {
-    offset: "50%"
-});
+    $(".js-wp-2").waypoint(function () {
+        $(".js-wp-2").addClass("animated fadeInUp");
+    }, {
+            offset: "50%"
+        });
+    $(".js-wp-3").waypoint(function () {
+        $(".js-wp-3").addClass("animated fadeIn");
+    }, {
+            offset: "50%"
+        });
+    $(".js-wp-4").waypoint(function () {
+        $(".js-wp-4").addClass("animated pulse");
+    }, {
+            offset: "50%"
+        });
 
-/* Mobile Navigtion */
-$(".js-nav-icon").click(function () {
-    var nav = $(".js-main-nav");
-    var icon = $(".js-nav-icon i");
+    /* Mobile Navigtion */
 
-    nav.slideToggle(200);
-    if (icon.hasClass("ion-navicon-round")) {
-        icon.addClass("ion-close-round");
-        icon.removeClass("ion-navicon-round");
-    } else {
-        icon.addClass("ion-navicon-round");
-        icon.removeClass("ion-close-round");
-    }
-})
+    $(".js-nav-icon").click(function () {
+        var nav = $(".js-main-nav");
+        var icon = $(".js-nav-icon i");
+
+        nav.slideToggle(200);
+        if (icon.hasClass("ion-navicon-round")) {
+            icon.addClass("ion-close-round");
+            icon.removeClass("ion-navicon-round");
+        } else {
+            icon.addClass("ion-navicon-round");
+            icon.removeClass("ion-close-round");
+        }
+    })
+
+    /* Maps */
+
+    var map = new GMaps({
+        div: '.map',
+        lat: 37.7778616,
+        lng: -122.30,
+        zoom: 12
+    });
+
+    map.addMarker({
+        lat: 37.777,
+        lng: -122.42,
+        title: ' Our San Francisco HQ',
+        // infoWindow: {
+        //     content: '<p> Our San Francisco HQ </p>'
+        //   }
+      });
 
 });
